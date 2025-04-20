e26f3268682811a82418be8ece89f34bcbbea690
Yuri Rocha <130370083+yrocha05@users.noreply.github.com>
2025-04-20 16:16:31 -0300

    Finalizado Básico

--- super_trunfo.c
+++ super_trunfo.c
@@ -7,12 +7,23 @@
 //Teste larissa
 
 int main() {
-    char estado [15] , sigla [5] , nome_cidade [30] ;
+
+    // Variaveis da 1 carta
+    char estado [20] , sigla [5] , nome_cidade [30] ;
     int  populacao ;
     float area ;
     float pib ; 
     int ponto_turistico;
 
+    //Variaveis da 2 carta
+
+    char estado2 [20] , sigla2 [10] , nome_cidade2 [30] ;
+    int  populacao2 ;
+    float area2 ;
+    float pib2 ; 
+    int ponto_turistico2;
+
+    printf("Carta 01\n");
     printf("Qual é o Estado da sua Carta? \n");
     scanf("%s" , &estado);
     
@@ -34,13 +45,50 @@ int main() {
     printf("Quantos Pontos Turisticos ? \n");
     scanf("%d" , &ponto_turistico);
 
+    //Questionário para cadastro da segunda carta 
+
+    printf("Carta 02\n");
+    printf("Qual é o Estado da sua Carta? \n");
+    scanf("%s" , &estado2);
+    
+    printf("Qual é a Sigla da carta? \n");
+    scanf("%s" , &sigla2);
+
+    printf("Qual é o Nome da Cidade? \n");
+    scanf("%s" , &nome_cidade2);
+    
+    printf("Qual é a Populacao? \n");
+    scanf("%d" , &populacao2);
+
+    printf("Qual é a área? \n");
+    scanf("%f" , &area2);
+
+    printf("Qual é o PIB? \n");
+    scanf("%f" , &pib2);
+
+    printf("Quantos Pontos Turisticos ? \n");
+    scanf("%d" , &ponto_turistico2);
+
+    // Exposição de dados das cartas
+
+    
+    printf("Carta 01 \n");
     printf("O Estado é : %s\n" , estado);
     printf("A Sigla é : %s\n" , sigla);
     printf("A Cidade é : %s\n" , nome_cidade);
     printf("A população é : %d\n" , populacao);
-    printf("A área é : %f\n" , area);
+    printf("A área é : %f Km² \n" , area);
     printf("O PIB é : %f\n" , pib);
     printf("A quantidade de ponto turístico é : %d\n" , ponto_turistico);
+
+    printf("Carta 02 \n");
+    printf("O Estado é : %s\n" , estado2);
+    printf("A Sigla é : %s\n" , sigla2);
+    printf("A Cidade é : %s\n" , nome_cidade2);
+    printf("A população é : %d\n" , populacao2);
+    printf("A área é : %f Km² \n" , area2);
+    printf("O PIB é : %f\n" , pib2);
+    printf("A quantidade de ponto turístico é : %d\n" , ponto_turistico2);
 
 
 
